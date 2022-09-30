5881f56d61224f79f0a96b866b31e1cf4632570f
mizki <wwr8462@gmail.com>
2022-10-01 02:45:11 +0900

    weather api

---
+++ weather/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Weather
+from .serializers import WeatherSerializer
+
+
+class TopView(ListView):
+    model = Weather
+    template_name = 'top.html'
+
+class WeatherAPIView(RetrieveAPIView):
+    queryset = Weather.objects.all()
+    serializer_class = WeatherSerializer
+    permission_classes = [IsAuthenticated]
